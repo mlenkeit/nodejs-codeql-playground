eaaba271d6c87293a74f08dd7ea5e1bfb93cc63e
Maximilian Lenkeit <maximilian.lenkeit@sap.com>
2024-07-12 11:44:57 +0200

    add cbom generation

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const crypto = require('crypto')
 
 const sha256FromString = (str) => {
-  const hash = crypto.createHash('sha256')
+  const hash = crypto.createHash('SHA256')
   const data = hash.update(Buffer.from(str, 'utf-8'))
   return data.digest()
 }
 
-console.log(sha256FromString('Hello World').toString('base64'))
+console.log(sha256FromString('Hello World').toString('hex'))
